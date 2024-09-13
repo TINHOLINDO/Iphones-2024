7bf8d82574a4de1c838b71ebf7c7827e54b54afc
TINHOLINDO <134516651+TINHOLINDO@users.noreply.github.com>
2024-09-13 10:41:17 -0300

    Add files via upload

--- iphone.py
+++ iphone.py
@@ -5,15 +5,20 @@ class iPhone:
         self.modelo = modelo
         self.ano_lancamento = ano_lancamento
         self.características = características
-        self.sistema_operacional = sistema_operacional
+        self._sistema_operacional = sistema_operacional
         iPhone.iphones.append(self)
 
-    def __str__(self):
-        return f'{self.modelo} | {self.ano_lancamento} | {self.características} | {self.sistema_operacional}'
-    
-    def listar_iPhone():
-        for iphone in iPhone.iphones:
-            print(f'{iphone.modelo} | {iphone.ano_lancamento} | {iphone.características} | {iphone.sistema_operacional}')
+    @property
+    def sistema_operacional(self):
+        return '🍎 Sistema operacional' if self._sistema_operacional else '🍏 Sistema operacional'
+
+    @classmethod
+    def listar_iPhone(cls):
+        print('')
+        print('''...''') 
+        print(f'{"Modelo do iphone".ljust(25)} | {"Ano de lançamento".ljust(22)} | {"Característica".ljust(22)} | {"Sistema operacional".ljust(22)}')
+        for iphone in cls.iphones:
+            print(f'{iphone.modelo.ljust(25)} | {str(iphone.ano_lancamento).ljust(22)} | {iphone.características.ljust(22)} | {iphone.sistema_operacional}')
 
 iphone_13 = iPhone('iPhone 13', 2021, 'Câmera dupla, Chip A15 Bionic, 5G', 'iOS 15')
 iphone_14 = iPhone('iPhone 14', 2022, 'Melhorias na câmera, Chip A16 Bionic, 5G', 'iOS 16')
